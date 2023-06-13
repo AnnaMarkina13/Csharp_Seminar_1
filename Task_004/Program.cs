d8da23029ab2e2cc676e526885e5e1d431b87c51
Anna Markina <latyshanna0@gmail.com>
2023-06-13 11:23:08 +0300

    Added Task_004

---
+++ Task_004/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), 
+// а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number < 2)
+{
+    System.Console.WriteLine("Число должно быть больше  1!");
+    return;
+}
+
+for (int i = 2; i <= number; i+=2)
+{
+    System.Console.Write($"{i} ");
+}
